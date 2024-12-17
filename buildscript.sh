adf194082057fb777f2d9639e586306bee2f9b32
dungna31 <nguyenanhdung281101@gmail.com>
2024-12-17 15:58:52 +0700

    update

---
+++ buildscript.sh
@@ -0,0 +1,16 @@
+cd prodcare_client
+sudo cp .env.prod .env
+npm run build
+sudo cp .env.dev .env
+cd ..
+sudo cp -r ./prodcare_client/build/* ./prodcare-server/prodcare-static/
+cd prodcare-server
+rm -f prodcare.tar
+sudo docker image rm prodcare
+sudo docker image prune -f
+sudo docker build -t prodcare .
+sudo docker save -o prodcare.tar prodcare:latest
+sudo openvpn
+sudo scp prodcare.tar developer@112.10.0.100:/home/developer/dungna31
+ssh -t developer@112.10.0.100 'cd /home/developer/dungna31/prodcare && chmod +x buildscript.sh && sudo ./buildscript.sh && exec bash'
+
